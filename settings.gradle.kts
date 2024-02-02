1cc4faf35a7999935549995cdd41aaef09f112f0
Jon Parker <jiparker@mitre.org>
2024-02-02 11:35:00 -0500

    feat: Adding gradle support

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+rootProject.name = "open-aria"
+
+include("open-aria-core")
+include("open-aria-threading")
+include("open-aria-pairing")
+include("open-aria-kafka")
+include("open-aria-system")
+include("open-aria-airborne")
+
+
+// Check that every subproject has a custom build file, based on the project name.
+//   Got this idiom from org.junit.jupiter's gradle setup. Avoid having numerous "build.gradle.kts" files.
+rootProject.children.forEach { project ->
+    project.buildFileName = "${project.name}.gradle.kts"
+    require(project.buildFile.isFile) {
+        "${project.buildFile} must exist"
+    }
+}
