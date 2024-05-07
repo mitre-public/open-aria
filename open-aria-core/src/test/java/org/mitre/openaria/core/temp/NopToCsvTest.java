951821a195089e74a1564ab4369c8391bb4d17a6
Jon Parker <98672124+politeRaccoon@users.noreply.github.com>
2024-05-06 21:00:06 -0400

    Evolving AriaCsv implementation (#16)
    * Deleted Positioner
    
    * Evolving AriaCsv implementation | Removing temporary work

--- open-aria-core/src/test/java/org/mitre/openaria/core/temp/NopToCsvTest.java
+++ open-aria-core/src/test/java/org/mitre/openaria/core/temp/NopToCsvTest.java
@@ -5,7 +5,7 @@ import static org.hamcrest.Matchers.closeTo;
 import static org.hamcrest.Matchers.is;
 
 import org.mitre.openaria.core.NopPoint;
-import org.mitre.openaria.core.data.CsvPoint;
+import org.mitre.openaria.core.formats.ariacsv.AriaCsvHit;
 
 import org.junit.jupiter.api.Test;
 
@@ -22,14 +22,14 @@ class NopToCsvTest {
         NopPoint centerPoint = NopPoint.from(CENTER_RH_MESSAGE);
         String csvString = NopToCsv.nopToAriaCsvFormat(CENTER_RH_MESSAGE);
 
-        CsvPoint pt = new CsvPoint(csvString);
+        AriaCsvHit pt = new AriaCsvHit(csvString);
 
         assertThat(pt.time(), is(centerPoint.time()));
         assertThat(pt.latitude(), is(centerPoint.latitude()));
         assertThat(pt.longitude(), is(centerPoint.longitude()));
         assertThat(pt.linkId(), is("ZLA-465"));
         assertThat(pt.altitude(), is(centerPoint.altitude()));
-        assertThat(pt.rawData(), is(",,2016-07-10T06:16:35Z,ZLA-465,33.4922,-118.1300,11000,W1JIXSxDZW50ZXIsWkxBX0IsMDctMTAtMjAxNiwwNjoxNjozNS4wMDAsU0tXNTg0MCxDUkoyLEwsNDcxMiwxMTAsMzU1LDEyNCwzMy40OTIyLC0xMTguMTMwMCw0NjUsLCwsLC8sLFpMQV9CLCwsLEQwNjA4LFNBTiwsSUZSLCw0NjUsMTM5NjM5MjM1NyxMQVgsLDExMC8vMTEwLCxMLDEsLCx7Ukh9"));
+        assertThat(pt.rawCsvText(), is(",,2016-07-10T06:16:35Z,ZLA-465,33.4922,-118.1300,11000,W1JIXSxDZW50ZXIsWkxBX0IsMDctMTAtMjAxNiwwNjoxNjozNS4wMDAsU0tXNTg0MCxDUkoyLEwsNDcxMiwxMTAsMzU1LDEyNCwzMy40OTIyLC0xMTguMTMwMCw0NjUsLCwsLC8sLFpMQV9CLCwsLEQwNjA4LFNBTiwsSUZSLCw0NjUsMTM5NjM5MjM1NyxMQVgsLDExMC8vMTEwLCxMLDEsLCx7Ukh9"));
     }
 
     @Test
@@ -37,14 +37,14 @@ class NopToCsvTest {
 
         NopPoint starsPoint = NopPoint.from(STARS_RH_MESSAGE);
         String csvString = NopToCsv.nopToAriaCsvFormat(STARS_RH_MESSAGE);
-        CsvPoint pt = new CsvPoint(csvString);
+        AriaCsvHit pt = new AriaCsvHit(csvString);
 
         assertThat(pt.time(), is(starsPoint.time()));
         assertThat(pt.latitude(), closeTo(starsPoint.latitude(), 0.0001));
         assertThat(pt.longitude(), closeTo(starsPoint.longitude(), 0.0001));
         assertThat(pt.linkId(), is("A80-1519"));
         assertThat(pt.altitude(), is(starsPoint.altitude()));
-        assertThat(pt.rawData(), is(",,2016-07-10T20:03:53.856Z,A80-1519,34.2772,-83.6359,15900,W1JIXSxTVEFSUyxBODBfQiwwNy8xMC8yMDE2LDIwOjAzOjUzLjg1NixEQUwyMDAsTUQ4OCxELDEzMTEsMTU5LDMzOSwyMjEsMDM0LjI3NzE5LC0wODMuNjM1OTEsMTUxOSwxMzExLDU3LjIwNzgsNjYuNjE4MSwxLEwsQSxBODAsLERSRSxBVEwsMjAwNixBVEwsQUNULElGUiwsMDE0NjUsLCwsLDI3TCxMLDEsLDAse1JIfQ"));
+        assertThat(pt.rawCsvText(), is(",,2016-07-10T20:03:53.856Z,A80-1519,34.2772,-83.6359,15900,W1JIXSxTVEFSUyxBODBfQiwwNy8xMC8yMDE2LDIwOjAzOjUzLjg1NixEQUwyMDAsTUQ4OCxELDEzMTEsMTU5LDMzOSwyMjEsMDM0LjI3NzE5LC0wODMuNjM1OTEsMTUxOSwxMzExLDU3LjIwNzgsNjYuNjE4MSwxLEwsQSxBODAsLERSRSxBVEwsMjAwNixBVEwsQUNULElGUiwsMDE0NjUsLCwsLDI3TCxMLDEsLDAse1JIfQ"));
     }
 
     @Test
@@ -52,14 +52,14 @@ class NopToCsvTest {
 
         NopPoint meartsPoint = NopPoint.from(MEARTS_RH_MESSAGE);
         String csvString = NopToCsv.nopToAriaCsvFormat(MEARTS_RH_MESSAGE);
-        CsvPoint pt = new CsvPoint(csvString);
+        AriaCsvHit pt = new AriaCsvHit(csvString);
 
         assertThat(pt.time(), is(meartsPoint.time()));
         assertThat(pt.latitude(), closeTo(meartsPoint.latitude(), 0.0001));
         assertThat(pt.longitude(), closeTo(meartsPoint.longitude(), 0.0001));
         assertThat(pt.linkId(), is("ZUA-257"));
         assertThat(pt.altitude(), is(meartsPoint.altitude()));
-        assertThat(pt.rawData(), is(",,2019-11-05T15:28:06.020Z,ZUA-257,11.6384,141.6778,40000,W1JIXSxNRUFSVFMsWlVBX0IsMTEtMDUtMjAxOSwxNToyODowNi4wMjAsVUFMMTg1LEI3MzcsTCwyNjQ2LDQwMCw0NTAsMjM5LDAxMS42Mzg0LDE0MS42Nzc4LDI1NywsNjcuNTAyODcsMTQ1LjkxNjksLFpVQS8xRiwsWlVBX0IsLCwsLCwsLCwsLCxFMTQzMCw0MDAvLzQwMCwsTCwxLHtSSH0"));
+        assertThat(pt.rawCsvText(), is(",,2019-11-05T15:28:06.020Z,ZUA-257,11.6384,141.6778,40000,W1JIXSxNRUFSVFMsWlVBX0IsMTEtMDUtMjAxOSwxNToyODowNi4wMjAsVUFMMTg1LEI3MzcsTCwyNjQ2LDQwMCw0NTAsMjM5LDAxMS42Mzg0LDE0MS42Nzc4LDI1NywsNjcuNTAyODcsMTQ1LjkxNjksLFpVQS8xRiwsWlVBX0IsLCwsLCwsLCwsLCxFMTQzMCw0MDAvLzQwMCwsTCwxLHtSSH0"));
     }
 
 }
