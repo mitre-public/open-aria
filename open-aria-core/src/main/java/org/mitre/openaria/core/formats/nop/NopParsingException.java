9dc92b8e13d5a1febc622fbc305cea9621b937ef
Jon Parker <jiparker@mitre.org>
2024-05-15 12:43:29 -0400

    delint: Small change set

--- open-aria-core/src/main/java/org/mitre/openaria/core/formats/nop/NopParsingException.java
+++ open-aria-core/src/main/java/org/mitre/openaria/core/formats/nop/NopParsingException.java
@@ -1,6 +1,8 @@
 package org.mitre.openaria.core.formats.nop;
 
 
+import java.io.Serial;
+
 public class NopParsingException extends RuntimeException {
 
     NopParsingException(String message, Throwable cause) {
@@ -11,6 +13,7 @@ public class NopParsingException extends RuntimeException {
         super(message);
     }
 
+    @Serial
     private static final long serialVersionUID = -3351727457440720460L;
 
 }
